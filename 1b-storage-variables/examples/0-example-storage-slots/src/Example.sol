752ba77814e0eeb15d6ae8a10cf148b9ee86ec16
Ras <siregar.riyando@gmail.com>
2024-04-17 18:18:51 +0800

    Practice Storage Variable

--- 1b-storage-variables/examples/0-example-storage-slots/src/Example.sol
+++ 1b-storage-variables/examples/0-example-storage-slots/src/Example.sol
@@ -5,16 +5,16 @@ import "forge-std/console.sol";
 
 contract Example {
     uint256 constant a = 55;
-    uint256 b; // storage slot - 0x0
-    bool c; // storage slot - 0x1
+    uint256 b = type(uint256).max; // storage slot - 0x0
+    bool c = false; // storage slot - 0x1
 
     constructor() {
         // SSTORE - store to some storage location
         // SLOAD - read from some storage location
-        bool x;
+        bytes32 x;
         assembly {
             x := sload(0x2)
         }
-        console.log(x);
+        console.logBytes32(x);
     }
 }
